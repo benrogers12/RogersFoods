776c8ebc0c9cbceba580b1d64f07f6c3344c5283
OpenShift System User <5743b88c2d5271c587000197@ex-std-node736.prod.rhcloud.com>
2017-09-08 20:16:25 -0400

    First Checkin

---
+++ ritters.php
@@ -0,0 +1,133 @@
+<html>
+<head>
+<meta charset=UTF-8>
+<title>Ritter's LLC DB</title>
+</head>
+<body>
+<form method='POST' action="/ritters.php">		<!-- Sets the post return action-->
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<INPUT TYPE = "Text" VALUE ="" NAME = "search">
+
+<input type="submit" name="submit" value="Search!" />		<!--Creates the Search button-->
+<br><strong>Search by entering the name or category in the search box</strong>
+<br><br>
+</form>
+
+<?php
+$link = mysqli_connect(localhost, "admin29Krb6S", "vCeKcj5hjSAt","ritters");
+if(mysqli_connect_errno()) {
+        echo "<p>Failed to connect to DB. Contact Ben Rogers.</p>";             //Echo's if the connection fails
+        exit();
+         }
+
+
+ if(isset($_POST['submit'])){           //Checks for the button to be pressed
+
+        $data=$_POST['search'];
+	//$query = "SELECT * FROM wholesale WHERE item LIKE '%$data%' OR cat LIKE '%$data%';";
+	$query = "SELECT * FROM CSVImport WHERE item LIKE '%$data%' OR cat LIKE '%$data%';";
+        $result = mysqli_query($link,$query) or die('Query failed: ');  //Checks for errors with the query 
+        $rows = mysqli_num_rows($result);                       //Counts the rows in the query
+        if($rows==0){
+                echo "There were no plants found.<br><br>";
+        }
+        else{
+		if($rows==1){
+                	echo "There was 1 plant found.<br><br>";
+        	}
+        	else{
+                	echo "There were $rows plants found <br><br>";
+        	}
+		//printf("Category Item Price 2wk Ready Quanity In Stock");echo "<br><br>";
+		echo '<table border=1><tr><th>Category</th><th>Item</th><th>Price</th><th>2 Weeks</th><th>Ready</th><th>Quantity In Stock</th>';
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+			echo '<tr><td>';
+        		printf($row['cat']);echo'</td><td>';
+			printf($row['item']);echo'</td><td>';
+			printf('$'.$row['price']);echo'</td><td>';
+			if($row['twowk'] == 1){printf("YES");}echo'</td><td>';
+			if($row['ready'] == 1){printf("YES");}echo'</td><td>';
+			printf($row['quanity']);
+			echo '</td></tr>';
+       		}
+		echo '</table>';
+	}
+        mysql_free_result($result);             //Free's the query
+ }
+echo '<br><br><br><br><br>';
+$query = "SELECT * FROM CSVImport";
+        $result = mysqli_query($link,$query) or die('Query failed: ');  //Checks for errors with the query 
+        $rows = mysqli_num_rows($result);                       //Counts the rows in the query
+        printf("Listed below is all of the inventory we currently have.");echo'<br><br><br>';
+                //printf("Category Item Price 2wk Ready Quanity In Stock");echo "<br><br>";
+                echo '<table border=4><tr><th>Category</th><th>Item</th><th>Price</th><th>2 Weeks</th><th>Ready</th><th>Quantity In Stock</th>';
+                while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+                        echo '<tr><td>';                        printf($row['cat']);echo'</td><td>';
+                        printf($row['item']);echo'</td><td>';
+                        printf('$'.$row['price']);echo'</td><td>';
+                        if($row['twowk'] == 1){printf("YES");}echo'</td><td>';
+                        if($row['ready'] == 1){printf("YES");}echo'</td><td>';
+                        printf($row['quanity']);
+                        echo '</td></tr>';
+                }
+                echo '</table>';
+        
+        mysql_free_result($result);             //Free's the query
+
+?>
+<!--
+<form method='POST' action="/index.php"> 
+<br><b>Enter a new recipe below.</b><br>
+Recipe Name: <INPUT TYPE = "Text" VALUE ="" NAME = "rName"><br>
+Category 1:  
+<select name="cat1">                            
+    <option value="Appetizer" >Appetizer</option>
+    <option value="Bread" >Bread</option>
+    <option value="Breakfast" >Breakfast</option>
+    <option value="Dessert" >Dessert</option>
+    <option value="Drink" >Drink</option>
+    <option value="Main Course" >Main Course</option>
+    <option value="Pasta" >Pasta</option>
+    <option value="Sauce" >Sauce</option>
+    <option value="Side Dish" >Side Dish</option>
+    <option value="Soups" >Soups</option>
+</select>
+<br>Category 2:  
+<select name="cat2">                            
+    <option value="Beef" >Beef</option>
+    <option value="Breads" >Breakfast</option>
+    <option value="Casserole" >Casserole</option>
+    <option value="Chicken" >Chicken</option>
+    <option value="Fish" >Fish</option>
+    <option value="Pork" >Pork</option>
+    <option value="Salad" >Salad</option>
+    <option value="Other" >Other</option>
+</select>
+<br>
+Ingredients: <textarea rows="15" cols="80" id="ingredients" NAME = "ingredients" VALUE = "">
+</textarea><br>
+Instructions: <textarea rows="20" cols="80" id ="instructions" VALUE ="" NAME = "instructions"></textarea><br><br>
+<input type="submit" name="add" value="Add New!" />
+</form>
+
+<br />-->
+<?php
+ if(isset($_POST['add'])){           //Checks for the button to be pressed
+                $name=$_POST['rName'];
+                $cat1=$_POST['cat1'];
+                $cat2=$_POST['cat2'];
+                $ingredients  = $_POST['ingredients'];
+                $ingredients  = nl2br($ingredients);
+                $instructions = $_POST['instructions'];
+                $instructions = nl2br($instructions);
+                $rowQuery = mysqli_query($link,"SELECT id FROM food;");
+                $id = mysqli_num_rows($rowQuery);
+                $query = "INSERT INTO food VALUES ('$id','$name','$cat1','$cat2','$ingredients','$instructions');";
+        $result = mysqli_query($link,$query) or die('Query failed: ');  //Checks for errors with the query 
+        mysql_free_result($rowQuery);
+        mysql_free_result($result);             //Free's the query
+ }
+mysql_close();		//Closes the connection to the databse
+?>
+</body>
+</html>
+
